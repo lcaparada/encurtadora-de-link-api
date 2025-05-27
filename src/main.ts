0b9053b78c02ecb1f365e5fdf6cc720d0a7bf960
lcaparada <lcaparada@gmail.com>
2025-05-27 11:16:36 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
